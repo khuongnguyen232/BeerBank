5f4d9e916d717424ebfdc7b54ea90b0157b3c021
TuringCom <softwarejobs@turing.com>
2019-04-06 01:37:44 -0700

    modify beer list

--- src/components/BeerList.js
+++ src/components/BeerList.js
@@ -1,48 +1,19 @@
 import React from 'react';
 import BeerCard from './BeerCard.js';
 
-class BeerList extends React.Component {
-  state = {beers : []};
+const BeerList = (props) => {
+  const list = props.allBeer.map((beer) => {
+    return <BeerCard key = {beer.id} beer = {beer} modifyFavoriteList = {props.modifyFavoriteList}/>
+  });
 
-  
-  componentWillMount(){
-    let list = this.props.allBeer.map((beer) => {
-      return <BeerCard key = {beer.id} beer = {beer} modifyFavoriteList = {this.props.modifyFavoriteList}/>
-    });
-    this.setState({beers:list});
+  if(list === null){
+    return<div>Loading ...</div>;
   }
-
-  //use this function when we have an update from parent App
-  componentDidUpdate(prevProps) {
-    if (this.props !== prevProps) {
-      //await this.props.loadBeer();
-      let list = this.props.allBeer.map((beer) => {
-        return <BeerCard key = {beer.id} beer = {beer} modifyFavoriteList = {this.props.modifyFavoriteList}/>
-      });
-      this.setState({beers:list});
-    }
-  }
-
-  renderContent = () => {
-    if(this.state.beers.length === 0){
-      return<div>Loading ...</div>;
-    }
-    return (
-        <div className = "ui three column grid">
-          {this.state.beers}
-        </div>
-      )
-    };
-
-
-
-  render() {
-    return(
-        <div>
-          {this.renderContent()}
-        </div>
-    );
-  };
+  return (
+      <div className = "ui three column grid">
+        {list}
+      </div>
+    )
 };
 
 export default BeerList;
